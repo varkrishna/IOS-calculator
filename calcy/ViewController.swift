bd388a5a28ec74d06d7b73eba3f871a25aa3ae28
VARSHNEY <varshney@VARSHNEYs-MacBook-Pro.local>
2016-06-29 11:45:41 +0530

    Initial Commit

---
+++ calcy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calcy
+//
+//  Created by VARSHNEY on 29/06/16.
+//  Copyright © 2016 VARSHNEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
